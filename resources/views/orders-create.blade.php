708f45d7e113ef592da88fd5b8371f0bc7d1ef1d
bishwanath-valedra <bishwanath@valedra.com>
2018-11-11 21:50:19 +0530

    Added view to create order

--- resources/views/orders-create.blade.php
+++ resources/views/orders-create.blade.php
@@ -20,9 +20,43 @@
                         </div>
                     @endif
 
-                        As we need a fixed height sidebar, we'll get rid of align-items property that stretched items vertically.
+                    <p>
+                        You can upload the orders data in given format, <a href="#">Download sample file</a>. It will automatically
+                        create orders, customers on shopify in some time using API.
+                    </p>
 
-                        However the content extends, the sidebar still will take entire viewport height. For this, we'll replace min-height: 100vh with height: 100vh.
+                    <form method="POST" action="/orders/create">
+                        @csrf
+                        <div class="form-group row"><label for="date" class="col-md-4 col-form-label text-md-right">Date</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" name="date" required="required" autofocus="autofocus" class="form-control" placeholder="dd/mm/yyyy">
+                            </div>
+                        </div>
+                        <div class="form-group row"><label for="end" class="col-md-4 col-form-label text-md-right">Select file</label>
+                            <div class="col-md-6">
+                                <input type="file" name="file" required="required" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row"><label for="cash-total" class="col-md-4 col-form-label text-md-right">Amount collected by cash</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" name="cash-total" required="required" autofocus="autofocus" class="form-control" >
+                            </div>
+                        </div>
+                        <div class="form-group row"><label class="col-md-4 col-form-label text-md-right">Amount collected by cheque</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" name="cheque-total" required="required" autofocus="autofocus" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row"><label class="col-md-4 col-form-label text-md-right">Amount collected by online</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" name="online-total" required="required" autofocus="autofocus" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0"><div class="col-md-6 offset-md-4"><button type="submit" class="btn btn-success">
+                                    Save
+                                </button></div></div>
+                    </form>
+
                 </div>
             </div>
     </div>
