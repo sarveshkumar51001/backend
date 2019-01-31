dc93c9c259c012e8cc16dd47083579b09d7063db
bishwanath-valedra <bishwanath@valedra.com>
2019-01-31 12:24:02 +0530

    Image reco views added

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -2,40 +2,42 @@
     <ul class="nav">
         <li class="nav-item">
             <a class="nav-link" href="{{ URL::to('/') }}"><i class="icon-speedometer"></i> Dashboard </a>
-            <li class="nav-title">Shopify</li>
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-database"></i> Shopify</a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/products') }}"><i class="fa fa-shopping-cart"></i>Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/customers') }}"><i class="icon-user"></i>Customers</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/orders') }}"><i class="fa fa-sitemap"></i>Orders</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-title">Customer profiler</li>
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-user"></i> Customer profiler</a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/customers/profiler') }}"><i class="icon-user"></i>Profiler result</a>
-                    </li>
-                </ul>
-            </li>
+        <li class="nav-title">Shopify</li>
+        <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-database"></i> Shopify</a>
+            <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/products') }}"><i class="fa fa-shopping-cart"></i>Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/customers') }}"><i class="icon-user"></i>Customers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/orders') }}"><i class="fa fa-sitemap"></i>Orders</a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-title">Customer profiler</li>
+        <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-user"></i> Customer profiler</a>
+            <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/customers/profiler') }}"><i class="icon-user"></i>Profiler result</a>
+                </li>
+            </ul>
+        </li>
 
-            <li class="nav-title">Image Recognitions</li>
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-search"></i> Search</a>
-                <ul class="nav-dropdown-items">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ URL::to('/imagereco/search') }}"><i class="fa fa-search"></i>By image</a>
-                    </li>
-                </ul>
-            </li>
+        <li class="nav-title">Image Recognitions</li>
+        <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-search"></i> Search</a>
+            <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('imagereco.list-all-people') }}"><i class="fa fa-search"></i>List All People</a>
+                    <a class="nav-link" href="{{ URL::to('/imagereco/search') }}"><i class="fa fa-search"></i>By image</a>
+                    <a class="nav-link" href="{{ route('imagereco.search-by-name') }}"><i class="fa fa-search"></i>By Name</a>
+                </li>
+            </ul>
+        </li>
         </li>
     </ul>
 </nav>
---
+++ resources/views/image-recognition/search-by-name.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.app')
+@section('content')
+
+    <div class = "card">
+        <div class="card-header">
+            <i class="fa fa-edit"></i> Search by Name
+        </div>
+        <form enctype="multipart/form-data" method="post" action="{{ route('imagereco.search-by-name-result')}}" id="add-convention-form" >
+            @csrf
+            <div class="card-body" >
+                <div>
+                    <label for="Name" id="" class="">Search Name : </label>
+                    <input id="" name="name" type="text" value="{{ old('name') }}">
+                </div>
+
+                <div>
+                    <label for="Organization" id="" class="">Tag : </label>
+                    <input id="" name="organization" type="text" value="">
+                </div>
+                <div>
+                    <button id="search-btn" type="submit" class="btn btn-sm btn-primary"><i class="icon-plus"></i> &nbsp; Search</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    @if(!empty($results))
+        <div class = "card">
+            <div class="card-body" >
+                @foreach($results as $result)
+                    <img src="{{ $result }}" width = "100">
+                @endforeach
+
+                @endif
+            </div></div>
+@endsection
+
