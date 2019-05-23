3337eca204cfaa6b97dfb4a21059555d3441aa88
bishwanath-valedra <bishwanath@valedra.com>
2019-05-23 12:25:21 +0530

    Slack alret added for exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,17 +4,21 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		AuthenticationException::class,
+		NotFoundHttpException::class
+	];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -26,17 +30,25 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception $exception
+	 *
+	 * @return void
+	 * @throws Exception
+	 */
+	public function report(Exception $exception)
+	{
+		parent::report($exception);
 
+		if ($this->shouldReport($exception) && !\App::isLocal()) {
+			// Run your custom code here
+			self::PostOnSlack(self::GetPayload($exception));
+		}
+	}
     /**
      * Render an exception into an HTTP response.
      *
@@ -48,4 +60,56 @@ class Handler extends ExceptionHandler
     {
         return parent::render($request, $exception);
     }
+
+	/**
+	 * Post message on slack
+	 *
+	 * @param string $payload
+	 */
+	private static function PostOnSlack(string $payload) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,"https://hooks.slack.com/services/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");
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_exec($ch);
+		curl_close($ch);
+	}
+
+	/**
+	 * @param Exception $exception
+	 *
+	 * @return string
+	 */
+	private static function GetPayload(Exception $exception) {
+		$attachments = [];
+
+		$attachments['title'] = 'An exception occurred on Backend server';
+		$attachments['color'] = '#FF0000';
+
+		$message = "EXCEPTION " . $exception->getMessage() . "\n";
+		$message .= "originates: " . $exception->getFile() . " (" . $exception->getLine() . ")\n";
+		$attachments['text'] = $message;
+
+		$data['time'] = date("c", time());
+		$data['env'] = env('APP_URL');
+		$data['url'] = request()->fullUrl();
+		$data['exception_class'] = get_class($exception);
+
+		$fields = [];
+		foreach ($data as $key => $value) {
+			$fields[] = [
+				'title' => $key,
+				'value' => $value,
+				'short' => true
+			];
+		}
+
+		$attachments['fields'] = $fields;
+
+		$payload['attachments'][] = $attachments;
+
+		return json_encode($payload);
+	}
 }
