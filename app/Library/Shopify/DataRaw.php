609fbf893b227a6b521b4456ced770be1487aed7
ishaan45 <ishaan.jain@valedra.com>
2019-07-04 11:37:53 +0530

    removed user_id logic

--- app/Library/Shopify/DataRaw.php
+++ app/Library/Shopify/DataRaw.php
@@ -147,10 +147,6 @@ class DataRaw
 			$order_data['tags'] .= ",installments";
 		}
 
-		if(!empty(\Auth::user()->shopify_id)){
-			$order_data['tags'] .= ", ".\Auth::user()->shopify_id;
-		}
-
 		$order_data['transactions'] = [[
 			"amount" => $this->data['final_fee_incl_gst'],
 			"kind" => "authorization"
