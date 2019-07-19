ee73d2e38f3e5f3ce04146fddf6881b11ea1bf75
daood <daood.shaheer@valedra.com>
2019-07-19 16:37:08 +0530

    product created/updated file & class renamed to create/update

--- app/Library/Webhook/Events/Shopify/ProductUpdate.php
+++ app/Library/Webhook/Events/Shopify/ProductUpdate.php
@@ -4,7 +4,7 @@ namespace App\Library\Webhook\Events\Shopify;
 use App\Models\Product;
 use App\Models\Webhook;
 
-class ProductUpdated
+class ProductUpdate
 {
 
     public static function handle(Webhook $Webhook)
