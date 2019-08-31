7deabf20894d560f4d460758a284756c67cec110
Ankur Goel <ankur@valedra.com>
2019-08-31 15:48:06 +0530

    Code Review changes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,23 @@
         </div>
         <div class="card-body">
             <div class="row col-md-12 justify-content-md-center">
-            <a href="{{ route('bulkupload.upload') }}" style="text-decoration: none;">
-                <div class="card col-md-12 p-0">
-                    <div class="card-body">
-                        <img src="{{asset('shopify/shopify-logo.png')}}" alt="shopify">
+                <div class="card col-md-2 m-1 p-0">
+                    <div class="card-body text-center">
+                        <a href="{{ route('bulkupload.upload') }}" class="stretched-link" style="text-decoration: none;">
+                        	<img src="{{asset('shopify/shopify-logo.png')}}" width="200px" alt="shopify">
+                    	</a>
+                    	
                     </div>
-                    <div class="card-footer text-center">Shopify Bulk Upload</div>
+                    <div class="card-footer text-center"><a href="{{ route('bulkupload.upload') }}" class="stretched-link" style="text-decoration: none;">Shopify Bulk Upload</a></div>
                 </div>
-            </a>
+            	<div class="card col-md-2 m-1 p-0">
+                    <div class="card-body text-center">
+                    	<a href="{{ route('search.students') }}" style="text-decoration: none;" class="stretched-link">
+                        	<i class="fa fa-search" aria-hidden="true" style="font-size:100px"></i>
+                    	</a>
+                    </div>
+                    <div class="card-footer text-center"><a href="{{ route('search.students') }}" style="text-decoration: none;" class="stretched-link">Student Search</a></div>
+               </div>
             </div>
         </div>
         </div>
