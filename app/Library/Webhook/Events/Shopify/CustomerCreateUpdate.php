da11be89fc567f0690c53e9f2242419f5319d3e9
ishaan.jain <ishaan.jain@valedra.com>
2019-10-07 13:37:54 +0530

    Merged customer create and update webhook

--- app/Library/Webhook/Events/Shopify/CustomerCreateUpdate.php
+++ app/Library/Webhook/Events/Shopify/CustomerCreateUpdate.php
@@ -6,14 +6,14 @@ use App\Library\Webhook\Channel;
 use App\Models\Webhook;
 use App\Models\Customer;
 
-class CustomerUpdate{
+class CustomerCreateUpdate{
 
     public static function handle(Webhook $Webhook)
     {
         $new_customer_data = $Webhook->body();
-        $product_id = $new_customer_data['id'];
+        $customer_id = $new_customer_data['id'];
 
-        if ($Customer = Customer::where('id', $product_id)->first()) {
+        if ($Customer = Customer::where('id', $customer_id)->first()) {
             $Customer->update($new_customer_data);
         } else {
             $domain_store = $Webhook->headers('x-shopify-shop-domain', null);
