a1cf6f43623f53ce8a2fe764384ef6d1e3fef330
ishaan.jain <ishaan.jain@valedra.com>
2019-11-27 16:29:45 +0530

    File added

---
+++ tests/Unit/ShopifyBulkUpload/HigherEducationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\ShopifyBulkUpload;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HigherEducationTest extends TestCase
+{
+    /**
+     * Test case for Higher Education related validations
+     */
+}
