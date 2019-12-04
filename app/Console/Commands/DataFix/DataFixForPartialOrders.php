9776673867c6a64ec99859ead0ecb18782350114
Ankur Goel <ankur@valedra.com>
2019-12-04 11:44:18 +0530

    Code review fixes.

--- app/Console/Commands/DataFix/DataFixForPartialOrders.php
+++ app/Console/Commands/DataFix/DataFixForPartialOrders.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Console\Commands\DataFix;
 
 use App\Jobs\ShopifyOrderCreation;
@@ -12,51 +13,8 @@ use App\Models\ShopifyExcelUpload;
 class DataFixForPartialOrders extends Command
 {
 
-    const ORDERS_LIST = [1339071856674,
-        1339072610338,
-        1339079196706,
-        1339079753762,
-        1339088568354,
-        1339092434978,
-        1339093123106,
-        1339095416866,
-        1339112456226,
-        1339112816674,
-        1339113340962,
-        1339113504802,
-        1339113734178,
-        1339114160162,
-        1339114782754,
-        1339115012130,
-        1339115241506,
-        1339115470882,
-        1339115733026,
-        1339115995170,
-        1339116159010,
-        1339116716066,
-        1339116879906,
-        1339116912674,
-        1339117109282,
-        1339117207586,
-        1339117404194,
-        1339117502498,
-        1339117764642,
-        1339117961250,
-        1339118125090,
-        1339118288930,
-        1339118485538,
-        1339119206434,
-        1339119927330,
-        1339120025634,
-        1339138441250,
-        1339138474018,
-        1339138637858,
-        1351923138594,
-        1351924449314,
-        1351924842530,
-        1351942012962,
-        1352070889506,
-        1352071479330
+    const ORDERS_LIST = [
+        //
     ];
     /**
      * The name and signature of the console command.
@@ -91,33 +49,35 @@ class DataFixForPartialOrders extends Command
     public function handle()
     {
 
+        $this->info("---- START OF PROCESS ---");
         $order_list = DataFixForPartialOrders::ORDERS_LIST;
-        $order_name = "";
 
-        foreach($order_list as $order_id){
+        foreach ($order_list as $order_id) {
 
             // Fetching ObjectID by order_id and applying unset and update operations
-            $Object = ShopifyExcelUpload::where('order_id',$order_id)->first();
-            $Object->unset('order_id')->update([ 'payments.0.processed'=> 'No', 'job_status' => 'pending']);
+            $Object = ShopifyExcelUpload::where('order_id', $order_id)->first();
+            $this->info("Processing Shopify Bulk Order " . $Object->_id);
+
+            $Object->unset('order_id')->update(['payments.0.processed' => 'No', 'job_status' => 'pending']);
 
             // Calling Job class run function and passing instance of raw data.
             $Data = new DataRaw(ShopifyExcelUpload::find($Object->_id)->toArray());
             $Order = Job::run($Data);
             $order_name = $Order['name'];
 
-            $this->info(sprintf('Created order with order name %s',$order_name));
+            $this->info(sprintf('New Order Created %s', $order_name));
 
             // Cancelling orders after replacement orders created on shopify.
-            $cancel_reason = sprintf("Cancel Reason: Incorrect order, new replacement order with name %s created on shopify.",$order_name);
+            $cancel_reason = sprintf("Cancel Reason: Incorrect order, new replacement order with name %s created on shopify.", $order_name);
             $ShopifyAPI = new API();
-            $ShopifyAPI->CancelOrder($order_id, []);
-            $ShopifyAPI->UpdateOrder($order_id,["note"=>$cancel_reason]);
+            $cancelled_order = $ShopifyAPI->CancelOrder($order_id, []);
+            $ShopifyAPI->UpdateOrder($order_id, ["note" => $cancel_reason]);
 
-            $this->info(sprintf('Canceled order with order id %s',$order_id));
-
+            $this->info(sprintf('Order ID %s [%s] canceled order', $order_id, $cancelled_order["name"]));
+            $this->comment("--------------------");
         }
-        $this->info("All false orders cancelled and replacement orders created successfully");
-        return ;
+        $this->info("---- END OF PROCESS ---");
+        return;
     }
 
 }
