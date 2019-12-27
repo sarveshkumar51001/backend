4a290c8da2a856214dc76b0ab8b07373e7b6135c
ishaan.jain <ishaan.jain@valedra.com>
2019-12-27 12:56:49 +0530

    added file download test cases

--- tests/Unit/ShopifyBulkUpload/FileDownloadTest.php
+++ tests/Unit/ShopifyBulkUpload/FileDownloadTest.php
@@ -2,20 +2,21 @@
 
 namespace Tests\Unit\ShopifyBulkUpload;
 
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\URL;
 use Tests\TestCase;
 
-class FileDownloadTest extends TestCase{
+class FileDownloadTest extends TestCase
 
-
+{
     public function testSampleFileDownload(){
-
-
+        $response = Response::download('public/shopify/sample_shopify_file.xls');
+        $this->assertEquals($response->getStatusCode(), 200);
     }
 
     public function testInstituteMappingDownload(){
-
+        $response = Response::download('public/shopify/Delivery_Institution_Details.xlsx');
+        $this->assertEquals($response->getStatusCode(), 200);
 
     }
-
-
 }
