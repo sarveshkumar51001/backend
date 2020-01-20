4860cb157dfd9c86dc1326909f164bed55f4b813
ishaan.jain <ishaan.jain@valedra.com>
2020-01-20 12:46:39 +0530

    changes according to the pull request

--- app/Console/Commands/TransferOrders.php
+++ app/Console/Commands/TransferOrders.php
@@ -42,23 +42,14 @@ class TransferOrders extends Command
     public function handle()
     {
         $file_data = [];
+        $file_id = $order_name = $order_id = "";
 
+        // Checking if the user wants to transfer a file or a single order and fetching owner accordingly
         if(empty($this->option('run'))) {
-            // Ask user to input the file id and fetch the file data..
-            $file_id = $this->ask('Please enter the file id');
-            $file_data = Upload::where('file_id', $file_id)->first();
-            if(empty($file_data)){
-                $this->error("The file provided does'not exists.");
-            }
-            // Fetch user who uploaded the file..
-            $User = User::where('_id', $file_data->user_id)->first();
-            $this->info(sprintf('This file was uploaded by %s', $User->name));
+            [$User,$file_id,$file_data] = $this->User_who_owns_file();
         } else {
-            $order_name = $this->option('run');
-            $user_id = ShopifyExcelUpload::where('shopify_order_name',$order_name)->first(['owner']);
-            $User = User::where('_id',$user_id)->first();
+            [$User,$order_name,$order_id] = $this->User_who_owns_order();
         }
-
         // Ask user to enter the email id to whom the transfer is to be done....
         $to_email = $this->ask('Please enter the email id of the user to whom you want to transfer.');
 
@@ -67,6 +58,64 @@ class TransferOrders extends Command
             $this->error('Transfer cannot take place to the user who owns the file/order. Please enter the correct email');
             return ;
         }
+        // Check if the email id provided for transfer exists or not....
+        $ToUser = User::where('email',$to_email)->first();
+        if(empty($ToUser)){
+            $this->error('Email address provided does not exists. Please check and try again.');
+            return ;
+        }
+        // If order name exists i.e. single order has to be transferred else complete file is to be transferred
+        if($order_name){
+            ShopifyExcelUpload::where('shopify_order_name',$order_name)->update(['owner' => $ToUser->_id]);
+            $orders[]['order_id'] = $order_id;
+        } else {
+            $this->Transfer_file_to_user($file_id,$file_data,$ToUser);
+            $orders = ShopifyExcelUpload::where('file_id',$file_id)->get(['order_id'])->toArray();
+        }
+        // Updating tags with new owner for each order in the file.
+        $this->Update_tags($orders,$ToUser);
+
+        $this->info("Transfer of order/file and update in shopify done successfully.");
+
+       return ;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function User_who_owns_order(){
+        // Ask user to input the order name of the order whose ownership needs to be transferred.
+        $order_name = $this->ask('Please enter the shopify order name');
+        $order = ShopifyExcelUpload::where('shopify_order_name',$order_name)->first(['owner','order_id']);
+
+        if(empty($order)){
+            $this->error("The order name doesn't exists.");
+            return [];
+        }
+        return [User::where('_id',$order['owner'])->first(),$order_name,$order['order_id']];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function User_who_owns_file()
+    {
+        // Ask user to input the file id and fetch the file data..
+        $file_id = $this->ask('Please enter the file id');
+        $file_data = Upload::where('file_id', $file_id)->first();
+
+        if(empty($file_data)){
+            $this->error("The file provided does'not exists.");
+            return [];
+        }
+        // Fetch user who uploaded the file..
+        $User = User::where('_id', $file_data->user_id)->first();
+        $this->info(sprintf('This file was uploaded by %s', $User->name));
+        return [$User,$file_id,$file_data];
+    }
+
+    private function Transfer_file_to_user($file_id,$file_data,$ToUser)
+    {
         // Fetching completed orders count for that file and matching with the total orders of that file and if matched
         // then proceed further otherwise throw an error.
         $orders_count = ShopifyExcelUpload::where('file_id', $file_id)->where('job_status', 'completed')->count();
@@ -74,32 +123,25 @@ class TransferOrders extends Command
             $this->error('File cannot be transferred as some of the orders are still due for completion.');
             return;
         }
-
         $this->alert(sprintf('Total number of entries to be affected are %s', $file_data->metadata['new_order']));
         // Asking user to choose whether to continue or not....
         $default_choice = 1;
         $choice = $this->choice('Do you wish to continue?', ['Yes', 'No'], $default_choice);
 
-        if($choice == 'No'){
+        if ($choice == 'No') {
             return;
-        }
-        $ToUser = User::where('email',$to_email)->first();
-
-        // Return error if no user found with the email address provided else transfer the file to the new owner.
-        if(empty($ToUser)){
-            $this->error('Email address provided does not exists. Please check and try again.');
-            return ;
-        }else {
+        } else {
             // Set a field owner and update it with the id of the user to whom the orders has to be transferred.
             ShopifyExcelUpload::where('file_id', $file_id)->update(['owner' => $ToUser->_id]);
         }
-        $this->info(sprintf('File transferred successfully. New owner of the file is %s.',$ToUser->name));
+        $this->info(sprintf('File transferred successfully. New owner of the file is %s.', $ToUser->name));
 
-        // Fetching orders list by file id....
-        $orders_list = ShopifyExcelUpload::where('file_id',$file_id)->get(['order_id'])->toArray();
+    }
 
-        // Fetch tags for the order change the owner email address and update order with tags....
-        foreach($orders_list as $order){
+    private function Update_tags($orders,$ToUser){
+
+        // Fetch tags for the order, change the owner email address and update order with tags....
+        foreach($orders as $order){
             $order_id = $order['order_id'];
 
             $shopifyAPI = new API();
@@ -114,8 +156,5 @@ class TransferOrders extends Command
             }
             $shopifyAPI->UpdateOrder($order_id,['tags' => implode(',',$tag_array)]);
         }
-        $this->info("Transfer of orders and update in shopify done successfully.");
-
-       return ;
     }
 }
