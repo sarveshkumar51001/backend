12a15d4f8385f6ee8a5557882a21cc9a35cbb213
ishaan.jain <ishaan.jain@valedra.com>
2020-02-04 15:20:54 +0530

    Added PDC filter

--- app/Http/Controllers/Api/CollectionController.php
+++ app/Http/Controllers/Api/CollectionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\ShopifyExcelUpload;
 use App\Http\Controllers\Controller;
+use App\User;
 use Carbon\Carbon;
 
 /**
@@ -27,8 +28,10 @@ class CollectionController extends Controller
         $daterange = [request('range_from'),request('range_to')];
         $location = request('location');
         $mode = request('mode');
+        $users = request('users');
+        $PDC = (request('pdc') == 'yes'? true:false);
 
-        return $this->GroupByData($daterange,$location,$mode);
+        return $this->GetJsonData($daterange,$location,$mode,$users,$PDC);
     }
 
     /**
@@ -36,9 +39,11 @@ class CollectionController extends Controller
      * @param $daterange
      * @param $location
      * @param $mode
+     * @param $users
+     * @param $PDC
      * @return array
      */
-    private function GetDocuments($daterange,$location,$mode)
+    private function GetDocuments($daterange,$location,$mode,$users,$PDC)
     {
         // If daterange is empty then assign current month as start and end time for fetching documents else
         // the daterange sent in request.
@@ -59,16 +64,21 @@ class CollectionController extends Controller
 
         // If not empty location then filter the documents on location.
         if (!empty($location)) {
-            $documents->where('student_school_location', $location);
+            $documents->whereIn('student_school_location', explode(',',$location));
         }
-
-//        if (!empty($mode)) {
-//            $documents->where('payments.mode_of_payment', $mode);
-//        }
-
+        if (!empty($mode)) {
+            $documents->where('payments.mode_of_payment', $mode);
+        }
+        if (!empty($users)){
+            $user_ids = array_column(User::whereIn('email',explode(',',$users))->get(['_id'])->toArray(),'_id');
+            $documents->whereIn('uploaded_by',$user_ids);
+        }
+        if (!empty($PDC)){
+            $documents->where('payments.is_pdc_payment',$PDC);
+        }
         // Fetch only required fields
-        $documents = $documents->get(['student_school_location', 'payments.amount', 'payments.processed'])->toArray();
-
+        $documents = $documents->get(['_id','student_school_location','payments.amount',
+            'payments.processed','payments.mode_of_payment','payments.is_pdc_payment'])->toArray();
         return [$documents,$start_month,$end_month];
 
     }
@@ -77,13 +87,14 @@ class CollectionController extends Controller
      * @param $daterange
      * @param $location
      * @param $mode
+     * @param $users
+     * @param $PDC
      * @return array
      */
-    private function GroupByData($daterange,$location,$mode)
+    private function GetJsonData($daterange,$location,$mode,$users,$PDC)
     {
-        $collection = [];
         $Collection_Data = [];
-        [$documents, $start_month, $end_month] = $this->GetDocuments($daterange, $location, $mode);
+        [$documents, $start_month, $end_month] = $this->GetDocuments($daterange, $location, $mode,$users,$PDC);
 
         $groupedData = GroupByKey($documents,'student_school_location');
 
@@ -92,18 +103,43 @@ class CollectionController extends Controller
             $Collection_Data['month'] = $start_month . '-' . $end_month;
         }
         foreach ($groupedData as $Data) {
-            $total_amount = 0;
-            foreach ($Data as $document) {
-                $processed_keys = array_keys(array_column($document['payments'], 'processed'), "Yes");
-                foreach ($processed_keys as $index => $key) {
-                    $amount = $document['payments'][$key]['amount'];
-                    $total_amount += $amount;
-                }
-                $collection['location'] = $document['student_school_location'];
-                $collection['amount'] = $total_amount;
-            }
+            $collection = $this->GetTotalAmount($Data,$mode,$PDC);
             $Collection_Data['collection'][] = $collection;
         }
         return $Collection_Data;
+    }
+
+    /**
+     * Function for getting total collection amount by adding only processed payments fetched after filter and filtering
+     * on the basis of payment mode and pdc payment if not empty.
+     *
+     * @param $Data
+     * @param $mode
+     * @param $PDC
+     * @return array
+     */
+    private function GetTotalAmount($Data,$mode,$PDC)
+    {
+        $collection = [];
+        $total_amount = 0;
+        foreach ($Data as $document) {
+            $processed_keys = array_keys(array_column($document['payments'], 'processed'), "Yes");
+            $keys = $processed_keys;
+            if(!empty($mode)){
+                $mode_keys = array_keys(array_column($document['payments'], 'mode_of_payment'), $mode);
+                $keys = array_intersect($processed_keys,$mode_keys);
+            }
+            if(!empty($PDC)){
+                $pdc_keys = array_keys(array_column($document['payments'],'is_pdc_payment'),$PDC);
+                $keys = (!empty($mode_keys) ? array_intersect($pdc_keys,$mode_keys) : $pdc_keys);
+            }
+            foreach ($keys as $index => $key) {
+                $amount = $document['payments'][$key]['amount'];
+                $total_amount += $amount;
+            }
+            $collection['location'] = $document['student_school_location'];
+            $collection['amount'] = $total_amount;
+        }
+        return $collection;
     }
 }
