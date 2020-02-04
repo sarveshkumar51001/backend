eb2ed47eed0ffd67963b4c1cf399b964a7300255
ishaan.jain <ishaan.jain@valedra.com>
2020-02-04 19:37:31 +0530

    api changes

--- app/Http/Controllers/Api/CollectionController.php
+++ app/Http/Controllers/Api/CollectionController.php
@@ -25,6 +25,7 @@ class CollectionController extends Controller
      */
     private function GetCollection()
     {
+        // request params
         $daterange = [request('range_from'),request('range_to')];
         $location = request('location');
         $mode = request('mode');
@@ -51,28 +52,34 @@ class CollectionController extends Controller
             $time_data = explode(',', Carbon::now()->format('F,Y'));
             $start_date = start_of_the_day(date('m/d/Y', strtotime(sprintf('first day of %s %s',$time_data[0], $time_data[1]))));
             $end_date = start_of_the_day(date('m/d/Y', strtotime(sprintf('last day of %s %s',$time_data[0],$time_data[1]))));
-            $start_month = $end_month = $time_data[0];
+            $start_month = $end_month = $time_data[0]." ".$time_data[1];
         } else {
+            // Fetching timestamps and month for start and end date.
             $start = Carbon::createFromFormat('d/m/Y', $daterange[0]);
             $end = Carbon::createFromFormat('d/m/Y', $daterange[1]);
             $start_date = $start->timestamp;
             $end_date = $end->timestamp;
-            $start_month = $start->monthName;
-            $end_month = $end->monthName;
+            $start_month = $start->monthName." ".$start->year;
+            $end_month = $end->monthName." ".$end->year;
         }
         $documents = ShopifyExcelUpload::whereBetween('payments.upload_date', [$start_date, $end_date]);
 
         // If not empty location then filter the documents on location.
+        // Location can be single as well as in the form of csv.
         if (!empty($location)) {
             $documents->whereIn('student_school_location', explode(',',$location));
         }
+        // If not empty mode then filter the documents on mode.
         if (!empty($mode)) {
             $documents->where('payments.mode_of_payment', $mode);
         }
+        // If not empty users then filter the documents on users.
+        // Users can be single as well as in the form of csv.
         if (!empty($users)){
             $user_ids = array_column(User::whereIn('email',explode(',',$users))->get(['_id'])->toArray(),'_id');
             $documents->whereIn('uploaded_by',$user_ids);
         }
+        // If not empty PDC then filter the documents on PDC status i.e. true/false.
         if (!empty($PDC)){
             $documents->where('payments.is_pdc_payment',$PDC);
         }
@@ -94,14 +101,18 @@ class CollectionController extends Controller
     private function GetJsonData($daterange,$location,$mode,$users,$PDC)
     {
         $Collection_Data = [];
+        //Fetching documents based on the filter parameters.
         [$documents, $start_month, $end_month] = $this->GetDocuments($daterange, $location, $mode,$users,$PDC);
 
+        // Grouping the documents by school location.
         $groupedData = GroupByKey($documents,'student_school_location');
 
+        // Set 'month' key to start if start and end are same else to start-end range.
         $Collection_Data['month'] = $start_month;
         if ($start_month != $end_month) {
             $Collection_Data['month'] = $start_month . '-' . $end_month;
         }
+        // Getting collected/due amount for the month location wise.
         foreach ($groupedData as $Data) {
             $collection = $this->GetTotalAmount($Data,$mode,$PDC);
             $Collection_Data['collection'][] = $collection;
@@ -125,14 +136,18 @@ class CollectionController extends Controller
         foreach ($Data as $document) {
             $processed_keys = array_keys(array_column($document['payments'], 'processed'), "Yes");
             $keys = $processed_keys;
+            // Finding matching payments i.e. payments which are processed as well as payment mode equal to $mode.
             if(!empty($mode)){
                 $mode_keys = array_keys(array_column($document['payments'], 'mode_of_payment'), $mode);
                 $keys = array_intersect($processed_keys,$mode_keys);
             }
+            // Finding matching payments in which PDC is true and mode is equal to $mode iff $mode is present else
+            // just return payments with PDC true.
             if(!empty($PDC)){
                 $pdc_keys = array_keys(array_column($document['payments'],'is_pdc_payment'),$PDC);
                 $keys = (!empty($mode_keys) ? array_intersect($pdc_keys,$mode_keys) : $pdc_keys);
             }
+            // Looping through all the keys obtained to calculate the total collected/due amount.
             foreach ($keys as $index => $key) {
                 $amount = $document['payments'][$key]['amount'];
                 $total_amount += $amount;
