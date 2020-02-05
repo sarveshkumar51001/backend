bcc118a3a8a17a681cf5940d7663684aaebcc6f2
bishwanath-valedra <bishwanath@valedra.com>
2020-02-05 12:01:22 +0530

    BA-158 Code audit changes

--- app/Http/Controllers/Api/CollectionController.php
+++ app/Http/Controllers/Api/CollectionController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\ShopifyExcelUpload;
-use App\Http\Controllers\Controller;
-use App\User;
 use Carbon\Carbon;
+use App\Library\Collection;
+use App\Http\Controllers\Controller;
 
 /**
  * Class CollectionController
@@ -13,148 +12,22 @@ use Carbon\Carbon;
  */
 class CollectionController extends Controller
 {
+    public function collection() {
+	    // Fetching timestamps and month for start and end date.
+	    $start = request('range_from') ? Carbon::createFromFormat('d/m/Y', request('range_from')) : Carbon::now()->startOfMonth();
+	    $end = request('range_to') ? Carbon::createFromFormat('d/m/Y', request('range_to')) : Carbon::now();
+	    $users = request('users') ? explode(',', request('users')) : [];
+	    $locationList = request('location') ? explode(',', request('location')) : [];
 
-    public function collection(){
+	    $Collection = new Collection();
+	    $data = $Collection->setStart($start)
+		    ->setEnd($end)
+		    ->setMode(request('mode'))
+		    ->setUsers($users)
+		    ->setLocation($locationList)
+		    ->setIsPDC(request('pdc') == 'yes')
+		    ->Get();
 
-        return $this->GetCollection();
-
-    }
-
-    /**
-     * @return mixed
-     */
-    private function GetCollection()
-    {
-        // request params
-        $daterange = [request('range_from'),request('range_to')];
-        $location = request('location');
-        $mode = request('mode');
-        $users = request('users');
-        $PDC = (request('pdc') == 'yes'? true:false);
-
-        return $this->GetJsonData($daterange,$location,$mode,$users,$PDC);
-    }
-
-    /**
-     *
-     * @param $daterange
-     * @param $location
-     * @param $mode
-     * @param $users
-     * @param $PDC
-     * @return array
-     */
-    private function GetDocuments($daterange,$location,$mode,$users,$PDC)
-    {
-        // If daterange is empty then assign current month as start and end time for fetching documents else
-        // the daterange sent in request.
-        if (empty($daterange[0]) || empty($daterange[1])) {
-            $time_data = explode(',', Carbon::now()->format('F,Y'));
-            $start_date = start_of_the_day(date('m/d/Y', strtotime(sprintf('first day of %s %s',$time_data[0], $time_data[1]))));
-            $end_date = start_of_the_day(date('m/d/Y', strtotime(sprintf('last day of %s %s',$time_data[0],$time_data[1]))));
-            $start_month = $end_month = $time_data[0]." ".$time_data[1];
-        } else {
-            // Fetching timestamps and month for start and end date.
-            $start = Carbon::createFromFormat('d/m/Y', $daterange[0]);
-            $end = Carbon::createFromFormat('d/m/Y', $daterange[1]);
-            $start_date = $start->timestamp;
-            $end_date = $end->timestamp;
-            $start_month = $start->monthName." ".$start->year;
-            $end_month = $end->monthName." ".$end->year;
-        }
-        $documents = ShopifyExcelUpload::whereBetween('payments.upload_date', [$start_date, $end_date]);
-
-        // If not empty location then filter the documents on location.
-        // Location can be single as well as in the form of csv.
-        if (!empty($location)) {
-            $documents->whereIn('student_school_location', explode(',',$location));
-        }
-        // If not empty mode then filter the documents on mode.
-        if (!empty($mode)) {
-            $documents->where('payments.mode_of_payment', $mode);
-        }
-        // If not empty users then filter the documents on users.
-        // Users can be single as well as in the form of csv.
-        if (!empty($users)){
-            $user_ids = array_column(User::whereIn('email',explode(',',$users))->get(['_id'])->toArray(),'_id');
-            $documents->whereIn('uploaded_by',$user_ids);
-        }
-        // If not empty PDC then filter the documents on PDC status i.e. true/false.
-        if (!empty($PDC)){
-            $documents->where('payments.is_pdc_payment',$PDC);
-        }
-        // Fetch only required fields
-        $documents = $documents->get(['_id','student_school_location','payments.amount',
-            'payments.processed','payments.mode_of_payment','payments.is_pdc_payment'])->toArray();
-        return [$documents,$start_month,$end_month];
-
-    }
-
-    /**
-     * @param $daterange
-     * @param $location
-     * @param $mode
-     * @param $users
-     * @param $PDC
-     * @return array
-     */
-    private function GetJsonData($daterange,$location,$mode,$users,$PDC)
-    {
-        $Collection_Data = [];
-        //Fetching documents based on the filter parameters.
-        [$documents, $start_month, $end_month] = $this->GetDocuments($daterange, $location, $mode,$users,$PDC);
-
-        // Grouping the documents by school location.
-        $groupedData = GroupByKey($documents,'student_school_location');
-
-        // Set 'month' key to start if start and end are same else to start-end range.
-        $Collection_Data['month'] = $start_month;
-        if ($start_month != $end_month) {
-            $Collection_Data['month'] = $start_month . '-' . $end_month;
-        }
-        // Getting collected/due amount for the month location wise.
-        foreach ($groupedData as $Data) {
-            $collection = $this->GetTotalAmount($Data,$mode,$PDC);
-            $Collection_Data['collection'][] = $collection;
-        }
-        return $Collection_Data;
-    }
-
-    /**
-     * Function for getting total collection amount by adding only processed payments fetched after filter and filtering
-     * on the basis of payment mode and pdc payment if not empty.
-     *
-     * @param $Data
-     * @param $mode
-     * @param $PDC
-     * @return array
-     */
-    private function GetTotalAmount($Data,$mode,$PDC)
-    {
-        $collection = [];
-        $total_amount = 0;
-        foreach ($Data as $document) {
-            $processed_keys = array_keys(array_column($document['payments'], 'processed'), "Yes");
-            $keys = $processed_keys;
-            // Finding matching payments i.e. payments which are processed as well as payment mode equal to $mode.
-            if(!empty($mode)){
-                $mode_keys = array_keys(array_column($document['payments'], 'mode_of_payment'), $mode);
-                $keys = array_intersect($processed_keys,$mode_keys);
-            }
-            // Finding matching payments in which PDC is true and mode is equal to $mode iff $mode is present else
-            // just return payments with PDC true.
-            if(!empty($PDC)){
-                $pdc_keys = array_keys(array_column($document['payments'],'is_pdc_payment'),$PDC);
-                $keys = (!empty($mode_keys) ? array_intersect($pdc_keys,$mode_keys) : $pdc_keys);
-            }
-            // Looping through all the keys obtained to calculate the total collected/due amount.
-            foreach ($keys as $index => $key) {
-                $amount = $document['payments'][$key]['amount'];
-                $total_amount += $amount;
-            }
-            $collection['location'] = $document['student_school_location'];
-            $collection['amount'] = $total_amount;
-        }
-        return $collection;
+        return $data;
     }
 }
---
+++ app/Library/Collection/Collection.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace App\Library;
+
+use App\User;
+use Carbon\Carbon;
+use App\Models\ShopifyExcelUpload;
+use Carbon\CarbonPeriod;
+
+/**
+ * Class Collection
+ */
+class Collection
+{
+	public $Start, $End, $mode;
+	public $users, $location = [];
+	public $results, $groupedData = [];
+	public $isPDC = true;
+
+	public $format = 'JSON';
+
+	private $columns = [
+		'_id',
+		'student_school_location',
+		'payments.amount',
+		'payments.processed',
+		'payments.mode_of_payment',
+		'payments.is_pdc_payment',
+		'payments.upload_date',
+	];
+
+	/**
+	 * Initialised the range default to current month
+	 */
+	public function __construct() {
+		$this->Start = Carbon::now()->startOfMonth();
+		$this->End   = Carbon::now();
+	}
+
+	public function HasTxnFilter() {
+		return ($this->isPDC == true || !empty($this->mode));
+	}
+
+	public function Get() {
+		$this->LoadFromDB();
+
+		$data = $this->Format();
+
+		return $data;
+	}
+
+	/**
+	 * @param mixed $Start
+	 *
+	 * @return Collection
+	 */
+	public function setStart(Carbon $Start) {
+		$this->Start = $Start;
+
+		return $this;
+	}
+
+	/**
+	 * @param mixed $End
+	 *
+	 * @return Collection
+	 */
+	public function setEnd(Carbon $End) {
+		$this->End = $End;
+
+		return $this;
+	}
+
+	/**
+	 * @param mixed $location
+	 *
+	 * @return Collection
+	 */
+	public function setLocation($location) {
+		$this->location = $location;
+
+		return $this;
+	}
+
+	/**
+	 * @param mixed $mode
+	 *
+	 * @return Collection
+	 */
+	public function setMode($mode) {
+		$this->mode = $mode;
+
+		return $this;
+	}
+
+	/**
+	 * @param array $users
+	 *
+	 * @return $this
+	 */
+	public function setUsers(array $users) {
+		$this->users = $users;
+
+		return $this;
+	}
+
+	private function LoadFromDB() {
+		// If daterange is empty then assign current month as start and end time for fetching documents else
+		// the daterange sent in request.
+		$documents = ShopifyExcelUpload::whereBetween('payments.upload_date', [$this->Start->timestamp, $this->End->timestamp]);
+
+		// If not empty location then filter the documents on location.
+		// Location can be single as well as in the form of csv.
+		if (!empty($this->location)) {
+			$documents->whereIn('student_school_location', $this->location);
+		}
+
+		// If not empty mode then filter the documents on mode.
+		if (!empty($this->mode)) {
+			$documents->where('payments.mode_of_payment', $this->mode);
+		}
+
+		// If not empty users then filter the documents on users.
+		// Users can be single as well as in the form of csv.
+		if (!empty($this->users)){
+			$userIDList = array_column(User::whereIn('email', $this->users)->get(['_id'])->toArray(),'_id');
+			$documents->whereIn('uploaded_by', $userIDList);
+		}
+
+		// If not empty PDC then filter the documents on PDC status i.e. true/false.
+		if ($this->isPDC == true){
+			$documents->where('payments.is_pdc_payment', true);
+		}
+
+		// Fetch only required fields
+		$this->results = $documents->get($this->columns);
+	}
+
+	/**
+	 *
+	 */
+	private function Format() {
+		$groupedData = [];
+		foreach ($this->results as $result) {
+			if (!isset($groupedData[$result->student_school_location])) {
+				$groupedData[$result->student_school_location] = [];
+			}
+
+			$monthlyTotal = $this->GetTotalAmountBreakUp($result->toArray());
+			foreach ($monthlyTotal as $month => $total) {
+				if (isset($groupedData[$result->student_school_location][$month])) {
+					$groupedData[$result->student_school_location][$month] += $total;
+				} else {
+					$groupedData[$result->student_school_location][$month] = $total;
+				}
+			}
+		}
+
+		$this->groupedData = $groupedData;
+
+
+		/**
+		 * [
+		 *   "Saket" => [
+		 *                  "January 2019": 20000,
+		 *                  "Feburary 2019" : 3000
+		 *
+		 *              ],
+		 *   "Pitumpura" => [
+		 *                  "January 2019": 20000,
+		 *                  "Feburary 2019" : 3000
+		 *
+		 *              ]
+		 * ]
+		 *
+		 */
+
+		if ($this->format == 'CSV') {
+			return $this->toCSV();
+		}
+
+		return $this->toJson();
+	}
+
+	/**
+	 * @param string $format
+	 *
+	 * @return Collection
+	 */
+	public function setFormat(string $format = 'JSON') {
+		$this->format = $format;
+
+		return $this;
+	}
+
+	private function toJson() {
+		$locations = array_keys($this->groupedData);
+
+		$jsonArray = [];
+		foreach (CarbonPeriod::create($this->Start, '1 month', $this->End) as $Month) {
+			$month = [];
+			$month['month'] = $Month->format('F Y');
+
+			$collection = [];
+			foreach ($locations as $location) {
+				$collection[] = [
+					'location' => $location,
+					'amount'   => $this->groupedData[$location][$Month->format('F Y')] ?? 0
+				];
+			}
+
+			$month['collection'] = $collection;
+
+			$jsonArray[] = $month;
+		}
+
+		return json_encode($jsonArray);
+
+	}
+
+	private function toCSV() {
+		$locations = array_keys($this->groupedData);
+
+		$csvList = [];
+		foreach (CarbonPeriod::create($this->Start, '1 month', $this->End) as $Month) {
+			$month = [];
+			foreach ($locations as $location) {
+				$month['month'] = $Month->format('F Y');
+				$month['location'] = $location;
+				$month['amount'] = $this->groupedData[$location][$Month->format('F Y')] ?? 0;
+			}
+
+			$csv[] = $month;
+		}
+
+		$header = ['month', 'location', 'total'];
+
+
+		return [$header, $csvList];
+	}
+
+	/**
+	 * @param bool $isPDC
+	 *
+	 * @return Collection
+	 */
+	public function setIsPDC( bool $isPDC ) {
+		$this->isPDC = $isPDC;
+
+		return $this;
+	}
+
+	/**
+	 * Getting total collection amount by adding only processed transactions also add transaction level filters
+	 *
+	 * @param array $document
+	 *
+	 * @return array
+	 * [
+	 *    "Janunary 2019" => 1000
+	 *    "Feburary 2019" => 2000
+	 * ]
+	 */
+	private function GetTotalAmountBreakUp(array $document)
+	{
+		$monthlyTotal = $final_keys = $mode_keys = $pdc_keys = [];
+		$processed_keys = array_keys(array_column($document['payments'], 'processed'), "Yes");
+
+		// Finding matching payments i.e. payments which are processed as well as payment mode equal to $mode.
+		if(!empty($this->mode)) {
+			$mode_keys = array_keys(array_column($document['payments'], 'mode_of_payment'), $this->mode);
+			$final_keys = array_merge($final_keys, $mode_keys);
+		}
+
+		// Finding matching payments in which PDC is true and mode is equal to $mode iff $mode is present else
+		// just return payments with PDC true.
+		if($this->isPDC) {
+			$pdc_keys = array_keys(array_column($document['payments'],'is_pdc_payment'), $this->isPDC);
+			$final_keys = array_merge($final_keys, $pdc_keys);
+		}
+
+		if (!$this->HasTxnFilter()) {
+			$final_keys = $processed_keys;
+		}
+
+		// Looping through all the keys obtained to calculate the total collected/due amount.
+		foreach ($final_keys as $key) {
+			$amount = $document['payments'][$key]['amount'];
+
+			$period = Carbon::createFromTimestamp($document['payments'][$key]['upload_date'])->format('F Y');
+			if (isset($monthlyTotal[$period])) {
+				$monthlyTotal[$period] += $amount;
+			} else {
+				$monthlyTotal[$period] = $amount;
+			}
+		}
+
+		return $monthlyTotal;
+	}
+}
