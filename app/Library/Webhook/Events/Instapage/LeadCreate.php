2215c02ecd2a1412c786a786bb794cb4e37c326d
bishwanath-valedra <bishwanath@valedra.com>
2020-03-24 13:35:59 +0530

    Sending email support added

--- app/Library/Webhook/Events/Instapage/LeadCreate.php
+++ app/Library/Webhook/Events/Instapage/LeadCreate.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Library\Webhook\Events\Instapage;
 
-use App\Library\Instapage\WebhookDataInstapage;
 use App\Library\Webhook\Channel;
+use Illuminate\Support\Facades\Mail;
+use App\Library\Instapage\WebhookDataInstapage;
 use App\Models\Webhook;
 use function slack;
 
@@ -11,6 +12,8 @@ class LeadCreate
     public static function handle(Webhook $Webhook)
     {
         self::postToSlack($Webhook);
+
+        self::sendEmail($Webhook);
     }
 
     private static function postToSlack(Webhook $Webhook)
@@ -27,5 +30,19 @@ class LeadCreate
                 ->success()
                 ->post();
         }
+    }
+
+    private static function sendEmail(Webhook $Webhook) {
+	    $body = $Webhook->body();
+
+	    $page_id = $body['page_id'];
+	    if ($page_id == 20189025)
+	    {
+	    	Mail::send('emails.20189025', $body, function ($message, $body) {
+			    $message->from('support@valedra.com', 'Valedra');
+			    $message->to($body['Email']);
+			    $message->attach(storage_path('files/Join Us via Zoom Call.pdf'));
+		    });
+	    }
     }
 }
