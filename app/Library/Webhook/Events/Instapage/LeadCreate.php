94d5403fcf2aa05ec443dec2b7c2ee0b5e161d2f
Ankur Goel <ankur@valedra.com>
2020-03-31 14:24:10 +0530

    Implemented Emailers for Yoga and Zumba Landing Pages

--- app/Library/Webhook/Events/Instapage/LeadCreate.php
+++ app/Library/Webhook/Events/Instapage/LeadCreate.php
@@ -13,15 +13,16 @@ class LeadCreate
     {
         self::postToSlack($Webhook);
 
-        self::sendEmail($Webhook);
+        self::sendEmail($Webhook, 1);
     }
 
     private static function postToSlack(Webhook $Webhook)
     {
+        return;
         $data = WebhookDataInstapage::getFormData($Webhook->body());
         $page_id = $Webhook->body()['page_id'];
         $title = ":tada: New Lead Captured from Page - ".$Webhook->body()['page_name'];
-        
+
         $channel = Channel::SlackUrl($page_id);
 
         foreach ($channel as $value) {
@@ -32,17 +33,44 @@ class LeadCreate
         }
     }
 
-    private static function sendEmail(Webhook $Webhook) {
+    private static function sendEmail(Webhook $Webhook, $sandbox = 0) {
 	    $body = $Webhook->body();
 
+	    $email = $body['Email'];
+	    if($sandbox) {
+	        $email = ['ankur@valedra.com', 'bishwanath@valedra.com', 'zuhaib@valedra.com', 'rhea@valedra.com'];
+        }
 	    $page_id = $body['page_id'];
+	    // events.valedra.com/online-yoga-at-home
 	    if ($page_id == 20189025)
 	    {
-	    	Mail::send('emails.20189025', $body, function ($message, $body) {
-			    $message->from('support@valedra.com', 'Valedra');
-			    $message->to($body['Email']);
-			    $message->attach(storage_path('files/Join Us via Zoom Call.pdf'));
-		    });
+//	    	Mail::send('emails.instapage.20189025', $body, function ($message, $body, $email) {
+//			    $message->from('support@valedra.com', 'Valedra');
+//			    $message->to($email);
+//			    $message->attach(storage_path('files/Join Us via Zoom Call.pdf'));
+//		    });
 	    }
+
+	    // https://events.valedra.com/online-yoga
+        // http://bit.ly/yoga-online-at-home
+        elseif ($page_id == 20202660)
+        {
+            Mail::send('emails.instapage.20202660', ['body' => $body], function ($message) use($email) {
+                $message->from('support@valedra.com', 'Valedra');
+                $message->to($email);
+                $message->attach(storage_path('files/Join Us via Zoom Call - Yoga.pdf'));
+            });
+        }
+
+        // https://events.valedra.com/zumba-at-home
+        // http://bit.ly/zumba-at-home
+        elseif ($page_id == 20221695)
+        {
+            Mail::send('emails.instapage.20221695', ['body' => $body], function ($message) use($email) {
+                $message->from('support@valedra.com', 'Valedra');
+                $message->to($email);
+                $message->attach(storage_path('files/Join Us via Zoom Call - Zumba.pdf'));
+            });
+        }
     }
 }
