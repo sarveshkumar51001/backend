abe96cec28d47d9914ccac3a827cfb50f328c3ce
Ankur Goel <ankur@valedra.com>
2020-04-01 13:13:44 +0530

    Fixed line breaks

--- resources/views/emails/instapage/20233330.blade.php
+++ resources/views/emails/instapage/20233330.blade.php
@@ -1,17 +1,12 @@
 <p>Dear {{ $body['Full Name'] }},</p>
 
-<br>
 <p>We hope you are doing well.</p>
 
-<br>
 <p>We are very excited to have you onboard for our online Indian Classical Dance session scheduled for 2nd April 2020 at 5:00 pm.</p>
 
-<br>
 <p>Please find attached the details for logging into the session. In case of any further assistance required, we are always happy to help, please feel free to reach out to us anytime on +91-9870155322, +91-8800708231.</p>
 
-<br>
 <p>See you!</p>
 
-<br>
 Take Care,<br>
 Team Valedra
