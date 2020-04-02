73e986ea8516b9906cf260cf0d14a95bc73b2a72
Ankur Goel <ankur@valedra.com>
2020-04-03 00:02:42 +0530

    Toppr emailer improvement

--- resources/views/emails/instapage/20238395.blade.php
+++ resources/views/emails/instapage/20238395.blade.php
@@ -2,10 +2,10 @@
 
 <p>Congratulations on successfully registering with us! You are now eligible to access Toppr's free subscription for two weeks with the access code mentioned below.</p>
 <p>To access the free subscription, please use code <strong>{{$code}}</strong>.</p>
-
+<br>
 <p><strong>Steps to Sign Up</strong></p>
 <p><strong>For Desktop</strong></p>
-<p>1. Click on the given link -https://www.toppr.com/signup/</p>
+<p>1. Click on the given link - <a href="https://www.toppr.com/signup/" target="_blank">https://www.toppr.com/signup/</a></p>
 <p>2. Fill in your details and click on "I have an invite code" and enter your access code</p>
 <p>3. Click on "Join Now"</p>
 <br>
@@ -14,12 +14,12 @@
 <p>2. Click on "Try for free"</p>
 <p>3. Fill in your details and click on "I have an invite code" and enter your access code</p>
 <p>4. Click on Sign up</p>
-
+<br>
 <p>In case of any further assistance required, we are always happy to help. Please feel free to reach out to us anytime on +91-9870155322, +91-8448388711.</p>
-
+<br>
 <p>Welcome on-board!</p>
-
+<br>
 <p>#StaySafeKeepStudying</p>
-
+<br>
 <p>Take Care,</p>
 <p>Team Valedra</p>
