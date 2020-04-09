2121823feb41463e48a773d76821de70adb2a533
sarveshkumar5100 <sarvesh@valedra.com>
2020-04-09 12:42:52 +0530

    Add InstaLeadsExport file

---
+++ app/Exports/InstaLeadsExport.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Exports;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+
+class InstaLeadsExport implements FromCollection,WithHeadings
+{
+    private $data;
+    use Exportable;
+
+    public function __construct($data =[]){
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        return collect($this->data);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Full Name',
+            'Email',
+            'Mobile',
+            'School'
+        ];
+    }
+}
