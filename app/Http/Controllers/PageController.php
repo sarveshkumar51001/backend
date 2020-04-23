50521bed5b86c9d07c0673f4e578b1f32ddbd25c
Ankur Goel <ankur@valedra.com>
2020-04-23 19:31:03 +0530

    Code Improvement

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,35 +20,31 @@ class PageController extends BaseController
         $InstaPage = [];
         $breadcrumb = ['List' => ''];
 
-        if (\Request::isMethod('get')) {
-
-            $page_id = !empty(request('page_id')) ? request('page_id') : '';
+        $page_id = request('page_id');
+        if (!empty($page_id)) {
             $date_params = getStartEndDate(request('daterange'));
             [$start_date, $end_date] = $date_params;
 
             $LeadsData = WebhookDataInstapage::getInstaPageList($start_date, $end_date, $page_id,WebhookDataInstapage::View);
             $InstaPage = InstaPage::where(InstaPage::PageId,$page_id)->first();
             $filename = !empty($page_id) ? sprintf("%s.xls", $InstaPage['page_name']) : '';
-
             // When excel is requested
-            if (!empty(request('download-csv'))) {
+            if (! empty(request('download-csv'))) {
                 $excel_data = [];
                 // data for Excel
-                $ExcelData = WebhookDataInstapage::getInstaPageList($start_date, $end_date, $page_id, WebhookDataInstapage::Excel);
-                $counter = 0;
-                foreach ($ExcelData as $data) {
-                    foreach ($InstaPage['lead_fields'] as $page => $key) {
-                        $keys[] = $key;
-                        if ($key == 'Captured At') {
-                            $excel_data[$counter]['Captured At'] = date("d-M-Y H:i:s", $data['created_at']);
-                        } else {
-                            $excel_data[$counter][$key] = $data['data']['body'][$key] ?? '';
+                if($ExcelData = WebhookDataInstapage::getInstaPageList($start_date, $end_date, $page_id, WebhookDataInstapage::Excel)) {
+                    $counter = 0;
+                    foreach ($ExcelData as $data) {
+                        foreach ($InstaPage['lead_fields'] as $page => $key) {
+                            $keys[] = $key;
+                            if ($key == 'Captured At') {
+                                $excel_data[$counter]['Captured At'] = date("d-M-Y H:i:s", $data['created_at']);
+                            } else {
+                                $excel_data[$counter][$key] = $data['data']['body'][$key] ?? '';
+                            }
                         }
+                        $counter++;
                     }
-                    $counter++;
-                }
-
-                if (!empty($excel_data)) {
                     return Excel\Facades\Excel::download(new InstaLeadsExport($excel_data), $filename);
                 }
             }
@@ -56,13 +52,11 @@ class PageController extends BaseController
 
         session()->flashInput(request()->input());
 
-        return view('pages.leads',
-            [  'Pages'=>$Pages,
-               'data' => $LeadsData,
-               'fields' => $InstaPage,
-               'breadcrumb' => $breadcrumb,
-               'ExcelData' => empty($excel_data),
-               'param' => request()->method()
-            ]);
+        return view('pages.leads', [
+            'Pages'=> $Pages,
+            'data' => $LeadsData,
+            'fields' => $InstaPage,
+            'breadcrumb' => $breadcrumb,
+        ]);
     }
 }
--- resources/views/pages/leads.blade.php
+++ resources/views/pages/leads.blade.php
@@ -5,8 +5,10 @@
         <div class="card-header">
             <i class="fa fa-file"></i>Leads Reports
         </div>
-        @if($ExcelData && request('download-csv'))
-            <h4 style="color: red"><b>No data found for excel export</b></h4>
+        @if(request('page_id') && empty($data->total()))
+            <div class="alert alert-danger" style="text-align: center">
+                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> No data found for the specified filter
+            </div>
         @endif
         <form method="get" action="{{route('pages.leads')}}"  id="report-form">
             <div class = "card-body">
@@ -19,7 +21,7 @@
                 @endif
                 <div class="row">
                     <div class="col-sm-4">
-                        <label><i class="fa fa-tag">Select Page</i></label>
+                        <label><i class="fa fa-tag"></i> Select Page</label>
                         <div class="input-group">
                             <select name="page_id" class="form-control select2" required="required">
                                 <option value="" selected disabled>Select Page Name </option>
@@ -30,7 +32,7 @@
                         </div>
                     </div>
                     <div class="col-sm-4">
-                        <label><i class="fa fa-calendar"> Period</i></label>
+                        <label><i class="fa fa-calendar"></i> Period</label>
                         <input id="txn_range" name="daterange" class="form-control date-picker" type="text" value="{{ request('daterange') }}">
                     </div>
                 </div>
@@ -42,7 +44,7 @@
             {{ csrf_field() }}
         </form>
     </div>
-    @if(!empty($data))
+    @if(request('page_id') && !empty($data->total()))
         <table class="table table-bordered table-striped table-sm datatable">
             <thead>
             <tr>
@@ -70,7 +72,5 @@
         <div class="row pull-right mr-4">
             {!! $data->appends(request()->query())->render() !!}
         </div>
-    @elseif(empty($data) && $param == 'get')
-     <h3 style="color: red"><b>No data found for the given period.</b></h3>
     @endif
 @endsection
