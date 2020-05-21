577b98b5e02362ac21f007a0d13c4a149915fa9b
sarveshkumar5100 <sarvesh@valedra.com>
2020-05-22 00:08:59 +0530

    Added new middleware and change css

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -80,11 +80,21 @@
         </li>
 
         @if(\Module::has('Online'))
-
-        <li class="nav-title"> Online</li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ URL::to('/online/activities') }}"><i class="fa fa-globe"></i> Activities </a>
-        </li>
+            <li class="nav-title"> Online</li>
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-upload"></i> Online</a>
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ URL::to('/online/activities') }}"><i class="fa fa-bookmark-o"></i> Activities </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ URL::to('/online/products') }}"><i class="fa fa-product-hunt"></i> Products </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ URL::to('/online/sessions') }}"><i class="fa fa-server"></i> Sessions </a>
+                    </li>
+                </ul>
+            </li>
         @endif
         </ul>
     </nav>
