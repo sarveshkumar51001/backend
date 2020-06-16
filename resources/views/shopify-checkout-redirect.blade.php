81bbf3a04be52ea26c2fb964f6a8264ebfb33871
ishaan.jain <ishaan.jain@valedra.com>
2020-06-16 15:34:54 +0530

    changes

--- resources/views/shopify-checkout-redirect.blade.php
+++ resources/views/shopify-checkout-redirect.blade.php
@@ -7,13 +7,18 @@
 </head>
 <body>
     <div class="row justify-content-center" style="top: 40%;left:30%;position: absolute">
+        <div class="box" style="display: flex;align-items: center">
+        <img src="{{asset("img/logo.png")}}">
         <div class="col">
-            @if(!empty($url))
-                <h4 class="alert-success"><b>Redirecting you to the Shopify Checkout page</b></h4>
+        @if(!empty($url))
+            <h4 class="alert-success"><b>Redirecting you to the Shopify Checkout page</b></h4>
                 <meta http-equiv = "refresh" content = "1; url = {{$url}}" />
-            @else
-                <h4 class="alert-danger"><b>Please select at least one subject in order to purchase the olympiad.</b></h4>
-            @endif
+        @else
+            <h4 class="alert-danger"><b>Please select at least one subject in order to purchase the olympiad.</b></h4>
+            <h5 class="alert-info" style="text-align: center">Redirecting you back to the form</h5>
+            <meta http-equiv = "refresh" content = "3; url = https://apeejay.formstack.com/forms/reynott_orange_olympiad" />
+        @endif
+        </div>
         </div>
     </div>
 </body>
