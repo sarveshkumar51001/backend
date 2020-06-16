5dc0237798151ca726359ea2f3b893171373d7cd
Ankur Goel <ankur@valedra.com>
2020-06-16 17:21:24 +0530

    Code Audit Fixes

--- resources/views/shopify-checkout-redirect.blade.php
+++ resources/views/shopify-checkout-redirect.blade.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="row justify-content-center" style="top: 40%;left:30%;position: absolute">
+    <div class="row justify-content-center mt-5 align-self-center">
+        <img src="{{asset("img/logo.png")}}" />
+    </div>
+    <div class="row justify-content-center mt-4">
         <div class="box" style="display: flex;align-items: center">
-        <img src="{{asset("img/logo.png")}}">
+
         <div class="col">
         @if(!empty($url))
-            <h4 class="alert-success"><b>Redirecting you to the Shopify Checkout page</b></h4>
-                <meta http-equiv = "refresh" content = "1; url = {{$url}}" />
+            <h4 class="alert-success"><b>Redirecting you to the Shopify Checkout page...</b></h4>
+                <meta http-equiv="refresh" content="0; url = {{$url}}" />
         @else
             <h4 class="alert-danger"><b>Please select at least one subject in order to purchase the olympiad.</b></h4>
-            <h5 class="alert-info" style="text-align: center">Redirecting you back to the form</h5>
-            <meta http-equiv = "refresh" content = "3; url = https://apeejay.formstack.com/forms/reynott_orange_olympiad" />
+            <h5 class="alert-info" style="text-align: center">Redirecting you back to the form...</h5>
+                <h6 style="text-align: center">If not redirected <a href="javascript:history.back()">Click Here</a></h6>
+            <script type="application/javascript">
+                setTimeout(function() { history.back(); }, 3000);
+            </script>
         @endif
         </div>
         </div>
