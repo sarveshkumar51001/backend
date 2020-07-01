7b409c2d429d1bc839d0c3392b4d974a52b807bc
Ankur Goel <ankur@valedra.com>
2020-07-01 16:52:34 +0530

    Added Delivery charges for redirect

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -27,6 +27,9 @@ class RedirectController
             $variant_test_book = 32809609101398;
             $variant_test = 32809609134166;
 
+            // Variant for Delivery Charges
+            $variant_delivery_charges_single = 32926112448598;
+            $variant_delivery_charges_multiple = 32926112481366;
 
             $url = "https://valedra.myshopify.com/cart/";
 
@@ -43,10 +46,18 @@ class RedirectController
                     }
                 }
 
+                if(!empty($data['deliver_books_to_residential_address']) && strtolower($data['deliver_books_to_residential_address']) == 'yes') {
+                    if($quant1 == 1) {
+                        $base_url .= ",$variant_delivery_charges_single:1";
+                    } elseif ($quant1 > 1) {
+                        $base_url .= ",$variant_delivery_charges_multiple:1";
+                    }
+                }
+
             $query_data = [
                 'checkout[email]' => $data['email'] ?? '',
-                'checkout[shipping_address][first_name]' => $data['mothers_name-first'] ?? '',
-                'checkout[shipping_address][last_name]' => $data['mothers_name-last'] ?? '',
+                'checkout[shipping_address][first_name]' => $data['parent_name-first'] ?? '',
+                'checkout[shipping_address][last_name]' => $data['parent_name-last'] ?? '',
                 'checkout[shipping_address][address1]' => $data['address-address'] ?? '',
                 'checkout[shipping_address][city]' => $data['address-city'] ?? '',
                 'checkout[shipping_address][state]' => $data['address-state'] ?? '',
