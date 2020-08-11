567104e951a259ee04a17ad719e693b072ba3fa5
sarveshkumar5100 <sarvesh@valedra.com>
2020-08-11 23:07:50 +0530

    Display Reconcile data on dashboard and filter result datewise

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Library\Shopify\Reconciliation\Payment;
 use App\Models\ShopifyExcelUpload;
 use Illuminate\Support\Facades\Auth;
+use \Carbon\Carbon;
 
 class HomeController extends BaseController
 {
@@ -15,7 +16,6 @@ class HomeController extends BaseController
      */
     public function index()
     {
-
 //    	$shopify = \Shopify::setShopUrl('valedra.myshopify.com')->setAccessToken(env('SHOPIFY_ACCESS_TOKEN'));
 //
 //    	// @see https://help.shopify.com/en/api/getting-started/search-syntax
@@ -23,7 +23,26 @@ class HomeController extends BaseController
 ////	    dd($shopify->get("admin/customers/search.json", ["query" => "email:bishwanathkj@gmail.com", "limit"=>20]));
 //	    dd($shopify->get("admin/customers/search.json", ["query" => "email:bishwanathkj@gmail.com OR phone:9899477299", "limit"=>20]));
 
-        $Orders = \DB::table('shopify_excel_uploads')->where('uploaded_by', Auth::id())->get();
+        //$Orders = \DB::table('shopify_excel_uploads')->where('uploaded_by', Auth::id())->get();
+        $OrderORM = ShopifyExcelUpload::orderBy('_id', 'DESC');
+        if(!empty(request('daterange'))) {
+            [$start_date,$end_date] = GetStartEndDate(request('daterange'));
+            $OrderORM->whereBetween('payments.upload_date',[$start_date,$end_date]);
+        }
+        else {
+            if(\Carbon\Carbon::now()->format('n') > 4) {
+               $year = Carbon::now()->format('Y');
+            }
+            else{
+                $year = Carbon::now()->format('Y') - 1;
+            }
+            $start_date = Carbon::parse('01-04-'.$year)->timestamp;
+            $end_date =  Carbon::parse(Carbon::now()->format('d-m-Y'))->timestamp;
+            $OrderORM->whereBetween('payments.upload_date',[$start_date,$end_date]);
+        }
+
+        $Orders = $OrderORM->get();
+        $Users = \DB::table('users')->orderBy('_id', Auth::id())->first();
 
         $reco_data = [
             'all' => [
@@ -65,6 +84,6 @@ class HomeController extends BaseController
         }
 
 
-        return view('home')->with('reco_data', $reco_data);
+        return view('home', ['reco_data'=> $reco_data, 'Users' => $Users]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,53 +34,91 @@
                 </div>
             </div>
 
-            @if($reco_data['all']['count'] > 0)
+            @if( isset( $Users['permissions'] ) && ( in_array('admin', $Users['permissions'])
+                || in_array('reconcile', $Users['permissions']))
+                )
+
                 <div class="card mt-4">
                     <div class="card-header">
                         <b><i class="fa fa-money"></i> Reconciliation Status</b>
                     </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="callout callout-info b-t-1 b-r-1 b-b-1">
-                                    <small class="text-muted">TOTAL TRANSACTIONS</small><br>
-                                    <strong class="h4">{{$reco_data['all']['count']}}</strong>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="callout callout-bordered b-t-1 b-r-1 b-b-1">
-                                    <small class="text-muted">TRANSACTION AMOUNT</small><br>
-                                    <strong class="h4">₹ {{amount_inr_format($reco_data['all']['amount'])}}</strong>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="row">
-                            @php
-                                $reco_statuses = [
-                                    'pending' => 'warning',
-                                    'settled' => 'success',
-                                    'returned' => 'danger'
-                                    ];
-                            @endphp
-                            @foreach($reco_statuses as $status => $color)
-                            <div class="col-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="text-value-lg text-{{$color}} font-weight-bold">{{ round($reco_data[$status ]['count'] / $reco_data['all']['count'] * 100,2) }}% ({{$reco_data[$status]['count']}})</div>
-                                        <div class="text-muted text-uppercase font-weight-bold small">{{ $status }} Transactions</div>
-                                        <div class="progress progress-xs my-2">
-                                            <div class="progress-bar bg-{{$color}}" role="progressbar" style="width: {{round($reco_data[$status]['count'] / $reco_data['all']['count'] * 100,2) }}%" aria-valuenow="{{ round($reco_data[$status]['count'] / $reco_data['all']['count'] * 100,2) }}" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div><div class="text-value-lg font-weight-bold text-{{$color}}">₹ {{amount_inr_format($reco_data[$status]['amount'])}}</div>
+                    <div class="card-body">
+                        @if(empty(request('daterange')))
+                            <span class="row alert alert-info">
+                                <i class="icon-info"></i>&nbsp;&nbsp;This data is showing for current financial Year
+                            </span>
+                        @endif
+                        @if($reco_data['all']['count'] > 0)
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <form method="GET" action="{{ route('home') }}">
+                                        <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-calendar"> Daterange</i></span>
+                                            <input id="txn_range" name="daterange" class="form-control date-picker" type="text">
+                                            <button  class="btn btn-success" style="margin-top: 0px;" type="submit">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-3">
+                                </div>
+                                <div class="col-md-3">
+                                    <a href="{{ URL::to('/transactions') }}" style="float: right;margin-left: 10px;margin-bottom: 20px;" class="btn btn-success">Transaction</a>
+                                    <a href="{{ URL::to('/shopify/reconcile') }}" style="float: right" class="btn btn-primary">Reconcile</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="callout callout-info b-t-1 b-r-1 b-b-1">
+                                        <small class="text-muted">TOTAL TRANSACTIONS</small><br>
+                                        <strong class="h4">{{$reco_data['all']['count']}}</strong>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="callout callout-bordered b-t-1 b-r-1 b-b-1">
+                                        <small class="text-muted">TRANSACTION AMOUNT</small><br>
+                                        <strong class="h4">₹ {{amount_inr_format($reco_data['all']['amount'])}}</strong>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
+                            <div class="row">
+                                @php
+                                    $reco_statuses = [
+                                        'pending' => 'warning',
+                                        'settled' => 'success',
+                                        'returned' => 'danger'
+                                        ];
+                                @endphp
+                                @foreach($reco_statuses as $status => $color)
+                                <div class="col-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="text-value-lg text-{{$color}} font-weight-bold">{{ round($reco_data[$status ]['count'] / $reco_data['all']['count'] * 100,2) }}% ({{$reco_data[$status]['count']}})</div>
+                                            <div class="text-muted text-uppercase font-weight-bold small">{{ $status }} Transactions</div>
+                                            <div class="progress progress-xs my-2">
+                                                <div class="progress-bar bg-{{$color}}" role="progressbar" style="width: {{round($reco_data[$status]['count'] / $reco_data['all']['count'] * 100,2) }}%" aria-valuenow="{{ round($reco_data[$status]['count'] / $reco_data['all']['count'] * 100,2) }}" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div><div class="text-value-lg font-weight-bold text-{{$color}}">₹ {{amount_inr_format($reco_data[$status]['amount'])}}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
 
-                        </div>
+                            </div>
+                        @else
+                            <div class="row alert alert-danger">
+                                Records Not Found
+                            </div>
+                        @endif
                     </div>
                 </div>
             @endif
-        </div>
+            </div>
         </div>
 @endsection
+<script>
+    function select_load(event)
+    {
+        window.location = "?year=" + event.value;
+        return false;
+    }
+</script>
