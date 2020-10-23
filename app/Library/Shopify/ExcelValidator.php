5335b05d825752c4587b2f8206b96f8bc2676018
Ankur Goel <ankur@valedra.com>
2020-10-23 20:46:59 +0530

    fixed canceled order check

--- app/Library/Shopify/ExcelValidator.php
+++ app/Library/Shopify/ExcelValidator.php
@@ -282,6 +282,7 @@ class ExcelValidator
 
             $DatabaseRow = ShopifyExcelUpload::where('date_of_enrollment', $date_enroll)->where('shopify_activity_id', $activity_id)
                 ->where('school_enrollment_no', $std_enroll_no)
+                ->where('is_canceled', '<>', true)
                 ->first();
 
             if (! empty($DatabaseRow)) {
